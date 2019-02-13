b1ddbe8d96a9da7c68fa707eec5248943719b10c
udayanid <udhayan.addon@gmail.com>
2019-02-13 10:07:46 +0530

    file renamed

---
+++ src/main/java/it/sella/UdayanBotApplication.java
@@ -0,0 +1,12 @@
+package it.sella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdayanBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdayanBotApplication.class, args);
+	}
+}
