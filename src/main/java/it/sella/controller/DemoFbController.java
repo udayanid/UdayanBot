7f7351fe9d16fc5e62f39487421a836306b6ca29
udayanid <udhayan.addon@gmail.com>
2019-02-19 10:03:07 +0530

    test

--- src/main/java/it/sella/controller/DemoFbController.java
+++ src/main/java/it/sella/controller/DemoFbController.java
@@ -39,7 +39,7 @@ public class DemoFbController {
 	Map<String, BotSession> botSessionMap = new HashMap<String, BotSession>();
     //https://sella.it/sellabot/chatinit?nome=nome1&cognome=cognome1&email=test@sella.it&CHANNEL=Sella_sito_free
 	private static final String SIGNATURE_HEADER_NAME = "X-Hub-Signature";
-	private static final String ACCESS_TOKEN = "EAAg3VtGDoZAQBAA0SbbxypnZCjhzPJ0EZAPMirlS3R0uotjHwkHX5rJEwavt4lUTZBuJKHmMZAFPLiOuIKm0rffGTbDJyuICuWVaY7SPoTI36BHQGFwPY6Ktlxh4P5QjWdqYrgucf4RzlJEVdgkQjSDRXb8uOjfRc4cEfKncMf8GRoLUgDDuR";
+	private static final String ACCESS_TOKEN = "EAAg3VtGDoZAQBAARsHWQi4GzMLAzHRJMP5CMb1AMLZBHmstBw9NxNYn1WmFofPJUNazxZBQhChNsynJLvgiTFfvI7xSXeJekTiZBbDuAgZAxTpTZA1T8gQSvsOqpJPD0ZCA5zHw9Kr1vr46V6E0oaRWfsUTeGdNSJnfqCLkzy6arC6PSKwdAXWE";
 	private static final String FB_GRAPH_API_URL_MESSAGES = "https://graph.facebook.com/v2.6/me/messages?access_token=%s";
 	private static final String IM_LOGIN_URL = "https://sella.it/sellabot/chatinit?nome=%s&cognome=%s&email=test3@sella.it&CHANNEL=Sella_sito_free";
 	private static final String CHAT_URL="https://sella.it/sellabot/execute/user/chat";
